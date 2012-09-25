d770d1558ec44764e6359164790b8e44df1578ef
brysj <brysj@brysj-VirtualBox.(none)>
2012-09-25 11:55:29 +0800

    first create

---
+++ .vim/c-support/templates/tst.cpp
@@ -0,0 +1,11 @@
+/**
+ *  Copyright (c) 20011—2012 Panguso.com. All rights reserved.
+ *  File Name:  tst.cpp
+ *  File Contents Outline :  XXXXXXXXX
+ *  File  Create Date:    2012年05月25日 08时40分18秒
+ *  File Version#: Revision#:  #1;#4
+ *  File  Latest Modify Date :   2012-05-22
+ *  File Author :  BriverSong (B.S), sayigood@gmail.com
+ **/
+
+
